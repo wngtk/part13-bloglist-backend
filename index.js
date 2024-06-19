ab11071aeb6978f788115c6e015a07f46be60f2d
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-06-19 17:01:54 +0800

    Exercise 13.1.: Using Render PostgreSQL

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import 'dotenv/config'
-import { Sequelize } from 'sequelize'
+import { DataTypes, Model, QueryTypes, Sequelize } from 'sequelize'
+import express from 'express'
+
+const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -10,14 +13,63 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   }
 })
 
+class Note extends Model {
+}
+
+Note.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  important: {
+    type: DataTypes.BOOLEAN
+  },
+  date: {
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'note'
+})
+
+app.use(express.json())
+
+app.get('/api/notes', async (req, res) => {
+  // const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+  const notes = await Note.findAll()
+  res.json(notes)
+})
+
+app.post('/api/notes', async (req, res) => {
+  console.log(req.body)
+  try {
+    const note = await Note.create(req.body);
+    return res.json(note);
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
 const main = async () => {
   try {
     await sequelize.authenticate()
     console.log('Connection has been established successfully.')
+    const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+    console.log(notes)
     await sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database:', error)
   }
 }
 
-main()
+app.listen(3003, async () => {
+  console.log('Server on 3003')
+  await sequelize.authenticate()
+})
